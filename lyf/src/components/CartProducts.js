5935ddf99a93107036d44e6233c4dc166da730ac
adamknapp124 <aknapp124@gmail.com>
2023-03-26 16:44:35 -0700

    Adds images to shopping cart

--- lyf/src/components/CartProducts.js
+++ lyf/src/components/CartProducts.js
@@ -3,6 +3,10 @@ import { CartContext } from '../CartContext';
 import { useContext } from 'react';
 import { getProductData } from '../data/products';
 
+import styles from '../scss/cartProducts.module.scss';
+
+import example from '../images/facial.jpg';
+
 export default function CartProduct(props) {
 	const cart = useContext(CartContext);
 	const id = props.id;
@@ -10,15 +14,21 @@ export default function CartProduct(props) {
 	const productData = getProductData(id);
 
 	return (
-		<>
-			<h3>{productData.name}</h3>
-			<p>{quantity} total</p>
-			<p>${(quantity * productData.price).toFixed(2)}</p>
-			{/* <img src={product.imgUrl} alt="" /> */}
+		<div className={styles.itemCard}>
+			<div className={styles.info}>
+				<div className={styles.cartThumbnail}>
+					<img src={productData.imgUrl} alt=""></img>
+				</div>
+				<div className={styles.itemDescription}>
+					<h3>{productData.name}</h3>
+					<div>{quantity} total</div>
+					<div>${(quantity * productData.price).toFixed(2)}</div>
+				</div>
+			</div>
 			<Button variant="outlined" onClick={() => cart.deleteFromCart(id)}>
 				Remove
 			</Button>
 			<hr></hr>
-		</>
+		</div>
 	);
 }
