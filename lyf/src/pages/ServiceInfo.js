497a40d018b15ef1f3efdb706cfa013da5906f47
adamknapp124 <aknapp124@gmail.com>
2023-05-01 21:37:18 -0700

    Wrapped serviceInfo in a container and applied some styles.

--- lyf/src/pages/ServiceInfo.js
+++ lyf/src/pages/ServiceInfo.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Container } from '@mui/system';
+import styles from '../scss/serviceInfo.module.scss';
 import { useParams } from 'react-router-dom';
+import formatCurrency from '../utilities/formatCurrency';
 
 import { getServices } from '../data/services';
 
@@ -11,19 +14,13 @@ export default function BrazilianInfo(props) {
 		return <div>Product not found</div>;
 	}
 	return (
-		<>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-			<div>{service.name}</div>
-		</>
+		<Container>
+			<div className={styles.serviceInfo}>
+				<div>{service.name}</div>
+				<img src={service.imageUrl} alt=""></img>
+				<div>{formatCurrency(service.price)}</div>
+				<div>{service.description}</div>
+			</div>
+		</Container>
 	);
 }
